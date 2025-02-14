d55379569826eaa384ebecd9531afb1932f075c4
Mateusz <mateusz.grzymowicz@gmail.com>
2025-02-14 16:10:15 +0100

    refactor: replace button with custom Buttin

--- frontend/src/components/file-table/file-action-menu/FileActionMenu.tsx
+++ frontend/src/components/file-table/file-action-menu/FileActionMenu.tsx
@@ -1,5 +1,5 @@
 import { FiMoreVertical } from "react-icons/fi";
-import { CircularProgress } from "@src/components/shared";
+import { Button, CircularProgress } from "@src/components/shared";
 import { useActionMenu } from "./hook/useActionMenu.hook";
 
 interface ActionMenuProps {
@@ -13,18 +13,17 @@ export function FileActionMenu(props: ActionMenuProps) {
 
   return (
     <div className="absolute right-0 mt-2 w-48 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded shadow-lg z-50">
-      <button
+      <Button
+        text={`Download ${name}`}
         onClick={onDownload}
-        className="w-full text-left px-4 py-2 text-green-500 hover:bg-green-100 dark:hover:bg-green-800 truncate"
-      >
-        Download {name}
-      </button>
-      <button
+        className="w-full text-left px-4 py-2 !text-green-500 hover:bg-green-100 dark:hover:bg-green-800 truncate bg-transparent"
+      />
+
+      <Button
+        text="Delete"
         onClick={onDelete}
-        className="w-full text-left px-4 py-2 text-red-500 hover:bg-red-100 dark:hover:bg-red-800"
-      >
-        Delete
-      </button>
+        className="w-full text-left px-4 py-2 !text-red-500 hover:bg-red-100 dark:hover:bg-red-800 bg-transparent"
+      />
     </div>
   );
 }

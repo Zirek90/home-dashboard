b1a0aa016d14459305fbea043927b556a6b17506
Mateusz <mateusz.grzymowicz@gmail.com>
2025-02-17 12:26:21 +0100

    fet: add new formatters to utils

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -4,3 +4,6 @@ export * from "./bytes-to-megabytes";
 export * from "./download-file";
 export * from "./get-icon-for-category";
 export * from "./format-date";
+export * from "./format-uptime";
+export * from "./format-usage";
+export * from "./format-bytes";
